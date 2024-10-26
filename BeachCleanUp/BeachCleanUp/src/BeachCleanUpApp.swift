eecae086b44777c46f43750f6226b784a7b02b9a
Yumeng Liu <lym20041026@gmail.com>
2024-10-25 17:46:21 -0700

    First commit

---
+++ BeachCleanUp/BeachCleanUp/src/BeachCleanUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeachCleanUpApp.swift
+//  BeachCleanUp
+//
+//  Created by Yumeng Liu on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BeachCleanUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeachCleanUp/BeachCleanUpTests/BeachCleanUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeachCleanUpTests.swift
+//  BeachCleanUpTests
+//
+//  Created by Yumeng Liu on 10/25/24.
+//
+
+import Testing
+@testable import BeachCleanUp
+
+struct BeachCleanUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
