c2b9564d9a8e28748e4b492f490eb626191e40cb
Yumeng Liu <lym20041026@gmail.com>
2024-11-20 13:43:15 -0800

    Added firebase

--- BeachCleanUp/BeachCleanUp/src/managers/FirebaseManager.swift
+++ BeachCleanUp/BeachCleanUp/src/managers/FirebaseManager.swift
@@ -6,17 +6,17 @@
 //
 
 import Foundation
-//import Firebase
-//import FirebaseFirestore
+import Firebase
+import FirebaseFirestore
 
-//class FirebaseManager: NSObject {
-//    let firestore: Firestore
-//        static let shared = FirebaseManager()
-//        
-//        override init() {
-//            FirebaseApp.configure()
-//            self.firestore = Firebase.Firestore.firestore()
-//            super.init()
-//        }
-//        
-//}
+class FirebaseManager: NSObject {
+    let firestore: Firestore
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        self.firestore = Firebase.Firestore.firestore()
+        super.init()
+    }
+        
+}
