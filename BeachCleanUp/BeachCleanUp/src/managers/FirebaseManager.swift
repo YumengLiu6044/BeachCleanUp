12092518196581d2cc26267eca1a3fdc2f8554dc
Yumeng Liu <lym20041026@gmail.com>
2024-11-22 20:46:28 -0800

    setup api call

--- BeachCleanUp/BeachCleanUp/src/managers/FirebaseManager.swift
+++ BeachCleanUp/BeachCleanUp/src/managers/FirebaseManager.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Firebase
 import FirebaseFirestore
+import FirebaseMLModelDownloader
 
 class FirebaseManager: NSObject {
     let firestore: Firestore
@@ -17,6 +18,24 @@ class FirebaseManager: NSObject {
         FirebaseApp.configure()
         self.firestore = Firebase.Firestore.firestore()
         super.init()
+    }
+    
+    static func loadTrashIdentifier(completion: @escaping (CustomModel?) -> ()) {
+        let downloadConditions = ModelDownloadConditions(allowsCellularAccess: false)
+        ModelDownloader.modelDownloader()
+            .getModel(name: "trash-classifier",
+                      downloadType: .latestModel,
+                      conditions: downloadConditions,
+                      progressHandler: { progress in
+                        // Handle progress.
+                        
+                      }) { result in
+                // Handle download result.
+                switch result {
+                case let .success(model): return completion(model) // Use model.
+                case let .failure(error): return completion(nil) // Handle error.
+                }
+            }
     }
         
 }
---
+++ BeachCleanUp/BeachCleanUp/src/managers/TrashManager.swift
@@ -0,0 +1,17 @@
+//
+//  TrashManager.swift
+//  BeachCleanUp
+//
+//  Created by Yumeng Liu on 11/20/24.
+//
+
+import SwiftUI
+import FirebaseMLModelDownloader
+
+class TrashClassifier {
+    private let api_address: String = ""
+    
+    static func requestPrediction() {
+        
+    }
+}
